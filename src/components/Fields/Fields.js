5346f2dcd2a6fef685044575d96eeedc2abe3247
lionofera <35335042+lionofera@users.noreply.github.com>
2022-10-11 20:51:23 +0200

    handler added to button

--- src/components/Fields/Fields.js
+++ src/components/Fields/Fields.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Fields.css';
 
 const Fields = ({field}) => {
-    const {name, logo, total} = field;
+    const {id, name, logo, total} = field;
+    const navigate = useNavigate();
+    const handleNavigate = () => {
+        navigate(`/fields/${id}`);
+    }
+
     return (
         <div className='m-3'>
             <img src={logo} alt=''></img>
             <div className=''>
                 <p className=''><span className='font-bold mr-6'>{name}</span><span className='text-sm'>No. of quiz: {total}</span></p>
-                <button className='btn'>Participate</button>
+                <button className='btn' onClick={handleNavigate}>Participate</button>
             </div>
 
         </div>
